fe3f3e6a5d4b1fdf7f8037513531219087d8610d
Mark Hoffmann <grizzo@gmail.com>
2018-01-08 05:08:18 -0800

    blazers colors tweak

--- src/components/TilesWrapper.js
+++ src/components/TilesWrapper.js
@@ -6,14 +6,19 @@ class TilesWrapper extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      image_loaded: 0
+    };
     this.class_names = "tiles_wrapper";
   }
 
   componentDidMount() {
+    console.log("Tiles Mounted");
     const img = new Image();
     img.onload = () => {
+      console.log("Image Loaded");
       this.class_names = "tiles_wrapper bg_loaded";
-      this.setState();
+      this.setState({image_loaded: 1});
     };
     if (window.innerWidth < 1200) {
       img.src = 'images/hubble_620.png';
@@ -24,7 +29,7 @@ class TilesWrapper extends React.Component {
 
   render() {
     return (
-        <section className={this.class_names}>
+        <section className={this.class_names} image_load={this.state.image_loaded}>
           <Tile title="professional interests" link="/professional" />
           <Tile tileClass="show_bg" />
           <Tile tileClass="show_bg" />
