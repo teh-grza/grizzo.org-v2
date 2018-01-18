7097951268bca9f03faa07ce0623109814fdfecf
Mark Hoffmann <grizzo@gmail.com>
2018-01-18 07:59:18 -0800

    fade in galaxy img + iframe updates

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -10,6 +10,7 @@ class NotFoundPage extends Component {
         <h4>If you were looking for the 404 page with a ridiculous Blackjack RPG on it, you're in the right place.  Otherwise, sorry, you're in the wrong place.</h4>
         <section style={{background: "rgba(255,255,255,.3)", padding: "1em 0"}} className="text_wrapper not-found">
           <Iframe src="Blackjack/www/index.html" width="100%" height="540" />
+          <p className="rmmv-message">Unfortunately, <a href="http://www.rpgmakerweb.com/products/programs/rpg-maker-mv">RMMV</a>'s canvas-based rendering loses touch / click support when sized responsively within a fluid iFrame.</p>
         </section>
       </div>
     )
