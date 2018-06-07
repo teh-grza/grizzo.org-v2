e417b0d62003e2e6645d45099e1473b7943d2ed0
Mark Hoffmann <grizzo@gmail.com>
2018-06-06 21:10:54 -0700

    lil update

--- src/pages/ProfessionalPage.js
+++ src/pages/ProfessionalPage.js
@@ -12,59 +12,55 @@ class ProfessionalPage extends Component {
     return (
       <div className="page professional_page">
         <PageHeader title="Professional Interests" subtitle="Strictly Business" />
-        <h4>Technical Management</h4>
-        <section className="text_wrapper tech-management">
-
-          <p>In a leadership position, I lead by example and hope to inspire others with hard work, awesome results, and decent jokes.</p>
-
-          <p>From 2011 until 2017 I managed a team of ~10 developers (20% of the company workforce) at <a href="http://www.wearefine.com/">FINE</a> while holding the company's most senior technical position. As manager of a team of developers my goal is to act as mentor, advocate and visionary for my team while supporting other managers and ensuring strong technical communication company-wide.  I consider myself an excellent teacher and I enjoy helping others reach their goals as much as I enjoy reaching my own.</p>
-
-          <p>When hiring, my goal is to assemble a team of extremely clever people with diverse backgrounds and experiences.  I prize communication, teamwork and other soft skills in addition to top-notch coding ability in potential hires.  I absolutely avoid cultural toxicity, striving for inclusion, efficiency, and honesty instead.</p>
-
-          <p>And not that anybody likes delivering bad news, but if somebody's got to do it, it should be me.  I can handle difficult conversations and defuse tense moments using charisma, logic or both.</p>
-
-          <p><a href="https://www.linkedin.com/in/mark-f-hoffmann/">LinkedIn</a> has more details of my experience, responsibilities and skills as a technical manager.</p>
-
-        </section>
 
         <h4>Full-stack web development</h4>
         <section className="text_wrapper full-stack">
           <p>I started building web pages for fun in 1995. I went pro in 2001, the same year as <a href="https://www.youtube.com/watch?v=ZS67ZPK5L2U">Gerald Wallace</a>.</p>
-          <dl>
-            <dt>2001</dt>
-            <dd>1st dev job: building .htm pages</dd>
-            <dt>2002</dt>
-            <dd>Freelance: static sites and Wordpress</dd>
-            <dt>2004</dt>
-            <dd>Hired by <a href="https://www.gravitatedesign.com/">Gravitate</a> as a Developer focused on CSS</dd>
-            <dt>2006</dt>
-            <dd>Shift to back-end focus: PHP, asp.net ecommerce builds</dd>
-            <dt>2007</dt>
-            <dd>Hired by <a href="http://www.wearefine.com/">FINE</a> as a Developer focused on CakePHP + jQuery</dd>
-            <dt>2009</dt>
-            <dd>Switch from SVN to Git</dd>
-            <dt>2010</dt>
-            <dd>Shift focus to Ruby on Rails + SCSS + jQuery</dd>
-            <dt>2011</dt>
-            <dd>Promoted to Director of Interactive Technology at FINE</dd>
-            <dt>2012</dt>
-            <dd><a href="https://github.com/wearefine/fae">FAE</a> is born</dd>
-            <dt>2015-ish</dt>
-            <dd>Shift focus to vanilla JS / frameworks</dd>
-            <dt>2017</dt>
-            <dd>Shift focus to Vue.js and React</dd>
-          </dl>
-
-          <p>Technologies/tools I've used professionally: HTML, CSS, JS, PHP, Ruby, ASP, ASP.net, SQL, jQuery, Vue, Visual Basic, XML, JSON, WordPress, CakePHP, Ruby on Rails, Slim, Capistrano, Jenkins, Visual Studio, Dreamweaver, Sublime Text, Atom, Flash, ImageMagick, Photoshop, ImageOptim, Word, Excel, PowerPoint, Google Suite, Redmine, Jira, Github, Bitbucket.</p>
+          {/*
+            <dl>
+              <dt>2001</dt>
+              <dd>1st dev job: building .htm pages</dd>
+              <dt>2002</dt>
+              <dd>Freelance: static sites and Wordpress</dd>
+              <dt>2004</dt>
+              <dd>Hired by <a href="https://www.gravitatedesign.com/">Gravitate</a> as a Developer focused on CSS</dd>
+              <dt>2006</dt>
+              <dd>Shift to back-end focus: PHP, asp.net ecommerce builds</dd>
+              <dt>2007</dt>
+              <dd>Hired by <a href="http://www.wearefine.com/">FINE</a> as a Developer focused on CakePHP + jQuery</dd>
+              <dt>2009</dt>
+              <dd>Switch from SVN to Git</dd>
+              <dt>2010</dt>
+              <dd>Shift focus to Ruby on Rails + SCSS + jQuery</dd>
+              <dt>2011</dt>
+              <dd>Promoted to Director of Interactive Technology at FINE</dd>
+              <dt>2012</dt>
+              <dd><a href="https://github.com/wearefine/fae">FAE</a> is born</dd>
+              <dt>2015-ish</dt>
+              <dd>Shift focus to vanilla JS / frameworks</dd>
+              <dt>2017</dt>
+              <dd>Shift focus to Vue.js and React</dd>
+            </dl>
+          */}
+          
+          <p>Technologies/tools I've used professionally: HTML, CSS, JS, PHP, Ruby, ASP, ASP.net, SQL, jQuery, Vue, Visual Basic, XML, JSON, WordPress, Drupal, Ruby on Rails, CakePHP, Slim, Capistrano, Jenkins, Visual Studio, Sublime Text, Atom, Flash, ImageMagick, Photoshop, ImageOptim, Word, Excel, PowerPoint, Google Suite, Redmine, Jira, Github, Bitbucket.</p>
 
           <p>So I've been a web dev for a long time and have worn many hats.  These days, I'm most excited about Progressive Web Apps, VR/AR, and React.</p>
-
 
         </section>
 
         <figure className="experience-skills">
           <ExperienceChart />
         </figure>
+
+        <h4>Technical Management</h4>
+        <section className="text_wrapper tech-management">
+
+          <p>In a leadership position, I lead by example and hope to inspire others with hard work, awesome results, and decent jokes.</p>
+
+          <p><a href="https://www.linkedin.com/in/mark-f-hoffmann/">LinkedIn</a> has more details of my experience, responsibilities and skills as a technical manager.</p>
+
+        </section>
 
         <h4>Game Development</h4>
         <section className="text_wrapper tech-management">
